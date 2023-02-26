9527d081a6357eedc3219c8f65df2cd5eec01af3
Muhammad Al Fauzi Darmawan <123710785+alfauzi140604@users.noreply.github.com>
2023-02-26 12:22:33 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+    double y, z;
+    char text1[]=    "\n\n \t\t\t Created By: ";
+    char text2[]=    "\n\n \t\t\t 672022027 - ADELINE FARADISIA";
+    char text3[]=    "\n\n \t\t\t 672022126 - M.RIZADRY EKA SYAPUTRA";
+    char text4[]=    "\n\n \t\t\t 672022146 - ROBBY ADRIAN FAJAR SULISTYA";
+    char text5[]=    "\n\n \t\t\t 672022186 - MUHAMMAD AL FAUZI DARMAWAN";
+    char text6[]=    "\n\n \t\t\t 672022188 - NADYA SALSABILA MUSTAFA";
+
+    printf("\n\n");
+        for(x=0; text1[x]!=NULL; x++)
+            {
+                printf("%c",text1[x]);
+                for(y=0; y<=8888888; y++);
+            }
+
+    printf("\n");
+        for(x=0; text2[x]!=NULL; x++)
+            {
+                printf("%c",text2[x]);
+                for(y=0; y<=8888888; y++);
+            }
+
+    printf("\n");
+        for(x=0; text3[x]!=NULL; x++)
+            {
+                printf("%c",text3[x]);
+                for(y=0; y<=8888888; y++);
+            }
+
+    printf("\n");
+        for(x=0; text4[x]!=NULL; x++)
+            {
+                printf("%c",text4[x]);
+                for(z=0; z<=8888888; z++);
+            }
+
+    printf("\n");
+        for(x=0; text5[x]!=NULL; x++)
+            {
+                printf("%c",text5[x]);
+                for(z=0; z<=8888888; z++);
+            }
+
+    printf("\n");
+        for(x=0; text6[x]!=NULL; x++)
+            {
+                printf("%c",text6[x]);
+                for(z=0; z<=8888888; z++);
+            }
+}
